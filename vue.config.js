aba8c271189d0b63385f8218a4cb28bafc0bf939
wlt <734490921@qq.com>
2022-07-13 20:13:19 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  // 是否开启eslint保存检测 ,它的有效值为 true || false || ‘error’
+  lintOnSave: false,
+  devServer: { host: 'localhost', port: 8080 }
+}
